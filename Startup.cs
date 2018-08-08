b74234878441d824a80409702a0e14e91e070efd
Mikael Araya <Mikaelaraya@example.com>
2018-08-08 23:24:25 +0300

    entity

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,16 @@
-using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using angularNet.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace angularNet
-{
+namespace angularNet    {
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -17,14 +18,17 @@ namespace angularNet
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { set; get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-          
-            services.AddMvc();
-
+            services.AddMvc( setupAction => 
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+            });
+             services.AddDbContext<smart_financeContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +47,7 @@ namespace angularNet
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
+        
 
             app.UseMvc(routes =>
             {
